3e91d8da6543d1444977f2b746d123063d4339c7
samkaj <kajavasamuel@gmail.com>
2023-10-22 21:09:40 +0200

    start game view clean up

--- frontend/src/game/game-view.ts
+++ frontend/src/game/game-view.ts
@@ -1,17 +1,19 @@
 import Scroller from "./scroller.js";
 import Game, { WordLimitGame } from "./game.js";
 
-const scroller = new Scroller("words");
+const input: any = document.getElementById("word-input");
 const up = document.getElementById("scroll-up");
 const down = document.getElementById("scroll-down");
-up?.addEventListener("click", scroller.up.bind(scroller));
-down?.addEventListener("click", scroller.down.bind(scroller));
-
-const game = new Game(new WordLimitGame(9));
+const game = new Game(new WordLimitGame(90));
 const gameBox = document.getElementById("words");
+const scroll = new Scroller("words");
+
+let activeY: number =
+    document.querySelector(".active")?.getBoundingClientRect().y || 0;
+
 if (gameBox) gameBox.innerHTML = game.getHtml();
-const input: any = document.getElementById("word-input");
-input?.addEventListener("input", (e: any) => {
+
+function handleInput(e: any) {
     const val = e.target.value;
     if (val.endsWith(" ")) {
         game.validateWord(val.trim());
@@ -21,13 +23,34 @@ input?.addEventListener("input", (e: any) => {
         if (game.isGameOver()) {
             alert("Game over!");
         }
+        scrollIfNewLine();
     }
-});
+}
 
-document.addEventListener("keydown", (e) => {
-    if (e.key === "Tab" || e.key === "Escape") {
+function scrollIfNewLine() {
+    const newActiveY =
+        document.querySelector(".active")?.getBoundingClientRect().y || 0;
+    if (activeY === 0) activeY = newActiveY;
+    if (newActiveY > activeY) {
+        scroll.down();
+        activeY = newActiveY;
+    }
+}
+
+function handleReset(e: any) {
+    if (e.key === "Escape") {
         game.reset();
         if (gameBox) gameBox.innerHTML = game.getHtml();
+        input.value = "";
         input.focus();
     }
-});
+}
+
+function addEventListeners() {
+    document.addEventListener("keydown", handleReset);
+    up?.addEventListener("click", scroll.up.bind(scroll));
+    down?.addEventListener("click", scroll.down.bind(scroll));
+    input?.addEventListener("input", handleInput);
+}
+
+addEventListeners();
